7d6d8861d0ac1c6bce6f4cf4076dd57ed87f3e5c
Angrycranberie <mathdep38@sfr.fr>
2020-03-31 20:58:55 +0200

    up

--- fichiers/echoserveri.c
+++ fichiers/echoserveri.c
@@ -12,7 +12,7 @@ pid_t proc[NPROC];
 void shutchildren(int sig) {
     int i;
     for(i=0; i<NPROC; i++){
-        Kill(proc[i], SIGINT);
+        Kill(proc[i], SIGKILL);
     }
     exit(0);
 }
@@ -30,7 +30,9 @@ int main(int argc, char **argv)
     struct sockaddr_in clientaddr;
     char client_ip_string[INET_ADDRSTRLEN];
     char client_hostname[MAX_NAME_LEN];
-
+    pid_t perePid = getpid();
+    Signal(SIGINT, shutchildren);
+    
     if (argc != 2) {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
@@ -43,30 +45,33 @@ int main(int argc, char **argv)
 
     int i;
     for(i=0; i<NPROC; i++){
-        if ((proc[i] = Fork()) != 0) { /* parent */
-            Signal(SIGINT, shutchildren);
-            while(1);
-        } else {
-            while (1) {
-                connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-
-            /* determine the name of the client */
-                Getnameinfo((SA *) &clientaddr, clientlen,
-                client_hostname, MAX_NAME_LEN, 0, 0, 0);
-
-            /* determine the textual representation of the client's IP address */
-                Inet_ntop(AF_INET, &clientaddr.sin_addr, client_ip_string,
-                    INET_ADDRSTRLEN);
-
-                printf("server connected to %s (%s)\n", client_hostname,
-                    client_ip_string);
-
-                echo(connfd);
-                Close(connfd);
-            }
-            exit(0);
-        }
+        if (getpid() == perePid) { /* parent */
+        proc[i] = Fork();
     }
-    return 0;
+}
+
+while (1) {
+    if(getpid() != perePid){
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+
+    /* determine the name of the client */
+        Getnameinfo((SA *) &clientaddr, clientlen,
+            client_hostname, MAX_NAME_LEN, 0, 0, 0);
+
+    /* determine the textual representation of the client's IP address */
+        Inet_ntop(AF_INET, &clientaddr.sin_addr, client_ip_string,
+            INET_ADDRSTRLEN);
+
+        printf("server connected to %s (%s)\n", client_hostname,
+            client_ip_string);
+
+        echo(connfd);
+        Close(connfd);
+    }
+}
+
+exit(0);
+
+return 0;
 }
 
